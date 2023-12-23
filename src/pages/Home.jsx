b7a3af0307dcf16dfe8122c5e0791098b3612575
NataFilina <filinanatash@gmail.com>
2023-12-23 12:24:26 +0200

    countries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,15 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
 import { useFetchCoutries } from 'hooks';
 
-
-
 export const Home = () => {
-
-const { counries,error, loading} = useFetchCoutries();
+  const { counries, error, loading } = useFetchCoutries();
 
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        {loading && <Loader />}
+        {error && <Heading textAlign="center">Something went wrong</Heading>}
+        {counries.length > 0 && <CountryList countries={counries} />}
       </Container>
     </Section>
   );
